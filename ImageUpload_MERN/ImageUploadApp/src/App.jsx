a1d34328a08dc63eca3b05fc4bdaee69ea008a47
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-07 23:15:27 -0500

    ImageUpload APP using MERN

---
+++ ImageUpload_MERN/ImageUploadApp/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>How to upload image and retrive image from backedn Database to UI</h2>
+    </>
+  );
+}
+
+export default App;
