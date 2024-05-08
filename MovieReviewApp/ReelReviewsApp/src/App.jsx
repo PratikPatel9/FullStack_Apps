f308c8b757765255f3fbe17dc6fb96aaf4eeac47
PatelPratik39 <ptl.pratik717@gmail.com>
2024-05-08 11:04:11 -0500

    tailwind works

---
+++ MovieReviewApp/ReelReviewsApp/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex justify-center h-screen items-center bg-blue-400">
+        <h1 className="text-7xl text-white">Reel Review App!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
